8d1e2112be7ea0187b005084d8896b05636efbf4
Raymond Thompson <60783171+Thealltommo@users.noreply.github.com>
2022-09-15 12:36:31 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+const randomDice = ["images/dice1.png", "images/dice2.png", "images/dice3.png", "images/dice4.png", "images/dice5.png", "images/dice6.png"];
+
+var randomNumber1 = Math.floor(Math.random() * randomDice.length);
+var randomNumber2 = Math.floor(Math.random() * randomDice.length);
+
+document.querySelector(".img1").src = randomDice[randomNumber1];
+document.querySelector(".img2").src = randomDice[randomNumber2];
+
+//If player 1 wins
+if (randomNumber1 > randomNumber2) {
+  document.querySelector("h1").innerHTML = "🚩 Play 1 Wins!";
+}
+else if (randomNumber2 > randomNumber1) {
+  document.querySelector("h1").innerHTML = "Player 2 Wins! 🚩";
+}
+else {
+  document.querySelector("h1").innerHTML = "Draw!";
+}
